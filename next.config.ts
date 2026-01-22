13b956fa55957139db8f1d80361e67782075bed1
whatup1359 <wutza_1779@hotmail.com>
2026-01-22 23:36:38 +0700

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/backend/:path*",
+        destination: "http://165.101.64.141/:path*",
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
